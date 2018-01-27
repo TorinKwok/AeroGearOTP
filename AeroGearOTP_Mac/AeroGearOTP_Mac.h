38e3951dc2c4b152d56bffc194f562e6bc9df5d5
Torin Kwok <torin@kwok.im>
2018-01-27 13:41:28 +0800

    Added compile taget for macOS

---
+++ AeroGearOTP_Mac/AeroGearOTP_Mac.h
@@ -0,0 +1,18 @@
+//
+//  AeroGearOTP_Mac.h
+//  AeroGearOTP_Mac
+//
+//  Created by Torin Kwok on 27/01/2018.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AeroGearOTP_Mac.
+FOUNDATION_EXPORT double AeroGearOTP_MacVersionNumber;
+
+//! Project version string for AeroGearOTP_Mac.
+FOUNDATION_EXPORT const unsigned char AeroGearOTP_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AeroGearOTP_Mac/PublicHeader.h>
+
+
